41bcfa0e56fd92494349e6a6847af7f0c13b3da3
Exoo25 <Exoo25pro@gmail.com>
2024-08-13 19:10:22 +0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,80 @@
+namespace SpriteKind {
+    export const player2 = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile2 = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 . . . . . . . . 
+        . . . . . . . 2 . . . . . . . . 
+        . . . . . . . 2 . . . . . . . . 
+        . . . . . . . 2 . . . . . . . . 
+        . . . . . . . 2 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 0, -50)
+    projectile2.setKind(SpriteKind.Projectile)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(sprite)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    scene.cameraShake(4, 500)
+    sprites.destroy(otherSprite)
+    info.changeLifeBy(-1)
+})
+let projectile: Sprite = null
+let projectile2: Sprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 4 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . e 4 . . . . . . . 
+    . . . . . . e e 5 2 . . . . . . 
+    . . . . . . e 4 5 2 . . . . . . 
+    . . . . . c c c 2 2 2 . . . . . 
+    . . . . e e 4 4 4 5 2 2 . . . . 
+    . . e f f f c c 2 2 f f 2 2 . . 
+    . e e e e 2 2 4 4 4 4 5 4 2 2 . 
+    e e e e e e 2 2 4 4 4 5 4 4 2 2 
+    e e e e e e 2 2 4 4 4 4 5 4 2 2 
+    `, SpriteKind.Player)
+mySprite.setStayInScreen(true)
+controller.moveSprite(mySprite)
+info.setLife(5)
+game.onUpdateInterval(1000, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . . . . c c 8 . . . . 
+        . . . . . . 8 c c c f 8 c c . . 
+        . . . c c 8 8 f c a f f f c c . 
+        . . c c c f f f c a a f f c c c 
+        8 c c c f f f f c c a a c 8 c c 
+        c c c b f f f 8 a c c a a a c c 
+        c a a b b 8 a b c c c c c c c c 
+        a f c a a b b a c c c c c f f c 
+        a 8 f c a a c c a c a c f f f c 
+        c a 8 a a c c c c a a f f f 8 a 
+        . a c a a c f f a a b 8 f f c a 
+        . . c c b a f f f a b b c c 6 c 
+        . . . c b b a f f 6 6 a b 6 c . 
+        . . . c c b b b 6 6 a c c c c . 
+        . . . . c c a b b c c c . . . . 
+        . . . . . c c c c c c . . . . . 
+        `, 0, 50)
+    projectile.x = randint(0, scene.screenWidth())
+    projectile.setKind(SpriteKind.Enemy)
+})
